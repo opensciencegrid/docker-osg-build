4e480d3a6642680a8269432d18f5b3d2f50ab0f4
Matyas Selmeci <matyas@cs.wisc.edu>
2023-09-23 05:34:28 -0500

    Add osg-build-shell for shelling into the osg-build container

--- input/command-wrapper.sh
+++ input/command-wrapper.sh
@@ -26,6 +26,22 @@ relpath () {
     python3 -c "import os,sys; print(os.path.relpath(sys.argv[1], sys.argv[2]))" "$1" "$2"
 }
 
+
+strict_work_dir=true
+if [[ $1 = "--no-strict-work-dir" ]]; then
+    # don't error out if we're outside the work directory
+    strict_work_dir=false
+    shift
+fi
+
+
+if [[ -x /home/build/work ]]; then
+    # we can't enter the work dir anyway; ignore it and just run the command
+    shift
+    exec "$@"
+fi
+
+
 outside_wd=$1
 shift
 
@@ -34,15 +50,20 @@ IFS=  read -r work_dir  </home/build/.work_dir
 inside_wd=$(relpath "$outside_wd" "$work_dir")
 
 set -e
-cd ~/work
 if [[ $inside_wd = ../* ]]; then
-    echo >&2 "The current directory is '$outside_wd'"
-    echo >&2 "which is not under the work directory '$work_dir'"
-    echo >&2 "commands must be run under the work directory"
-    exit 2
+    if $strict_work_dir; then
+        echo >&2 "The current directory is '$outside_wd'"
+        echo >&2 "which is not under the work directory '$work_dir'"
+        echo >&2 "commands must be run under the work directory"
+        exit 2
+    fi
+else
+    cd /home/build/work/"$inside_wd"
 fi
-cd "$inside_wd"
-get_proxy_if_needed
+
+if [[ $1 == "osg-build" || $1 == "osg-koji" ]]; then
+    get_proxy_if_needed
+fi
 
 if [[ ${KOJI_HUB} ]]; then
     sed -i -e "s/^srv = .*/srv = ${KOJI_HUB}/" /home/build/.osg-koji/config
